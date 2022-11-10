ad6c9ef0762a19558aae75247d932d764994c850
Haliboss <halibos@gmail.com>
2022-11-10 06:23:58 +0300

    add global settings

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyles } from "../components/GlobalStyles/GlobalStyles";
+import Navbar from "../components/nav/Navbar";
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
-  )
-}
+    <BrowserRouter>
+    <GlobalStyles/>
+      <Navbar />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
